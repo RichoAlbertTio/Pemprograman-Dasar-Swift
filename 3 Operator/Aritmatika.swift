8b871e1b9cccff8acfa1fe7fb0198d63eab565ae
Richo Albert Tio <101497830+RichoAlbertTio@users.noreply.github.com>
2022-11-26 00:03:58 +0700

    Add files via upload

---
+++ 3 Operator/Aritmatika.swift
@@ -0,0 +1,16 @@
+var a = 62.3 
+var b = 51.9 
+
+var hasil: Double 
+
+hasil = a + b 
+print("Hasil dari a + b adalah ", hasil) 
+
+hasil = a - b 
+print("Hasil dari a - b adalah ", hasil) 
+
+hasil = a * b 
+print("Hasil dari a * b adalah ", hasil) 
+
+hasil = a / b 
+print("Hasil dari a / b adalah ", hasil) 
