b2c8ee19685e5a36c813fc101fe37d546b71bda0
Richo Albert Tio <101497830+RichoAlbertTio@users.noreply.github.com>
2022-11-27 13:20:59 +0700

    Add files via upload

---
+++ Tugas Akhir/KonversiSuhu.swift
@@ -0,0 +1,13 @@
+// Mendeklarasikan variabel
+var c: Double, f: Double, r: Double
+// Memberikan inputan celcius 
+print("Masukan Suhu Celcius =")
+c = Double(readLine()!)!
+// rumus
+f = (9/5*c) + 32
+r = 4/5*c
+// Menampilkan konversi suhu celcius ke fahrenheit dan reamour
+print("Fahrenheit =", f)
+print("Reamour    =", r)
+
+
